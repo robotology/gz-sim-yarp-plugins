d794df152a1a18cfc9c7c71166262216c7f06ef1
lucapa17 <lucapastore2000@gmail.com>
2023-04-19 17:24:21 +0200

    Simple plugin Hello World

---
+++ forcetorque/build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/lucap/OneDrive - unige.it/Documenti/Università/Magistrale/Software Engineering/SE_Project/study-gazebo-garden-yarp-plugins/forcetorque/HelloWorld.cc" "CMakeFiles/HelloWorld.dir/HelloWorld.cc.o" "gcc" "CMakeFiles/HelloWorld.dir/HelloWorld.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ forcetorque/build/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/HelloWorld.cc.o"
+  "CMakeFiles/HelloWorld.dir/HelloWorld.cc.o.d"
+  "libHelloWorld.pdb"
+  "libHelloWorld.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
