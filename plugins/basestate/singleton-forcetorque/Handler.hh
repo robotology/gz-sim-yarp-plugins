529250f49f9f950ed58ee226967c249488c447ec
Alessandro Croci <alessandro.croci@iit.it>
2024-01-15 14:30:15 +0100

    Create basestate plugin folder by copying from forcetorque

---
+++ plugins/basestate/singleton-forcetorque/Handler.hh
@@ -0,0 +1,38 @@
+#include <array>
+#include <gz/common/Event.hh>
+#include <mutex>
+#include <string>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/PolyDriverList.h>
+
+struct ForceTorqueData
+{
+    std::mutex m_mutex;
+    std::array<double, 6> m_data;
+    std::string sensorScopedName;
+    double simTime;
+};
+
+namespace gzyarp
+{
+
+class HandlerForceTorque
+{
+public:
+    static HandlerForceTorque* getHandler();
+
+    bool setSensor(ForceTorqueData* _sensorDataPtr);
+
+    ForceTorqueData* getSensor(const std::string& sensorScopedName) const;
+
+    void removeSensor(const std::string& sensorName);
+
+private:
+    HandlerForceTorque();
+    static HandlerForceTorque* s_handle;
+    static std::mutex& mutex();
+    typedef std::map<std::string, ForceTorqueData*> SensorsMap;
+    SensorsMap m_sensorsMap; // map of known sensors
+};
+
+} // namespace gzyarp
