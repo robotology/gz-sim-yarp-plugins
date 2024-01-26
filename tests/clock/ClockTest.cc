79b33d991d374b269f722482a0388b8dd36a41bc
Alessandro Croci <alessandro.croci@iit.it>
2024-01-26 17:22:54 +0100

    Add unit test for clock plugin

---
+++ tests/clock/ClockTest.cc
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <gz/sim/TestFixture.hh>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Network.h>
+
+TEST(ClockTest, GetSimTimeAsExpected)
+{
+    // ARRANGE
+    yarp::os::NetworkBase::setLocalMode(true);
+    // Maximum verbosity helps with debugging
+    gz::common::Console::SetVerbosity(4);
+    // Instantiate test fixture
+    gz::sim::TestFixture fixture("../../../tests/clock/model.sdf");
+    const int iterations = 10;
+    const int deltaTns = 1e6;
+    yarp::os::BufferedPort<yarp::os::Bottle> p; // Create a port.
+    p.open("/tmp_in"); // Give it a name on the network.
+    yarp::os::Network::connect("/clock", "/tmp_in"); // connect two ports.
+
+    // ACT
+    fixture.Server()->Run(/*_blocking=*/true, iterations, /*_paused=*/false);
+
+    // ASSERT
+    yarp::os::Bottle* b = p.read();
+    auto simTime = b->get(0).asInt64();
+
+    ASSERT_NEAR(simTime, (iterations - 1) * deltaTns, 1000) << "simTime: " << simTime << std::endl;
+}
