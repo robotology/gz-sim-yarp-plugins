4a06b08fa297f69f3959402f0ecd1d0dd23963c7
Alessandro Croci <alessandro.croci@iit.it>
2024-01-26 17:52:04 +0100

    Update unit test

--- tests/clock/ClockTest.cc
+++ tests/clock/ClockTest.cc
@@ -13,17 +13,20 @@ TEST(ClockTest, GetSimTimeAsExpected)
     // Instantiate test fixture
     gz::sim::TestFixture fixture("../../../tests/clock/model.sdf");
     const int iterations = 10;
-    const int deltaTns = 1e6;
+    const int deltaTns = 1e6; // 1ms
+    const int tolerance = 1e6; // 1ms
     yarp::os::BufferedPort<yarp::os::Bottle> p; // Create a port.
     p.open("/tmp_in"); // Give it a name on the network.
     yarp::os::Network::connect("/clock", "/tmp_in"); // connect two ports.
 
     // ACT
     fixture.Server()->Run(/*_blocking=*/true, iterations, /*_paused=*/false);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     // ASSERT
     yarp::os::Bottle* b = p.read();
     auto simTime = b->get(0).asInt64();
 
-    ASSERT_NEAR(simTime, (iterations - 1) * deltaTns, 1000) << "simTime: " << simTime << std::endl;
+    ASSERT_NEAR(simTime, (iterations - 1) * deltaTns, tolerance)
+        << "simTime: " << simTime << std::endl;
 }
