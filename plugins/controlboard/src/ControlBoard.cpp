7a8d55be58f4679ae2758246c0cb584c1745d4f9
Alessandro Croci <alessandro.croci@iit.it>
2024-02-02 12:45:01 +0100

    Add Configure() implementation in ControlBoard.cpp

--- plugins/controlboard/src/ControlBoard.cpp
+++ plugins/controlboard/src/ControlBoard.cpp
@@ -1,7 +1,15 @@
 #include "../include/ControlBoard.hh"
+
 #include "../../../libraries/singleton-devices/Handler.hh"
+#include "../include/ControlBoardDriver.hh"
 
 #include <gz/plugin/Register.hh>
+#include <gz/sim/EntityComponentManager.hh>
+#include <gz/sim/Model.hh>
+#include <gz/sim/Util.hh>
+
+#include <yarp/os/Log.h>
+#include <yarp/os/LogStream.h>
 
 using namespace gz;
 using namespace sim;
@@ -27,7 +35,7 @@ ControlBoard::~ControlBoard()
     {
         m_controlBoardDriver.close();
     }
-    ControlBoardDataSingleton::getControlBoardHandler()->removeControlBoard();
+    // ControlBoardDataSingleton::getControlBoardHandler()->removeControlBoard();
 }
 
 // configure
@@ -36,32 +44,66 @@ void ControlBoard::Configure(const Entity& _entity,
                              EntityComponentManager& _ecm,
                              EventManager& _eventMgr)
 {
-    // TODO
+    using ControlBoardDriverCreator
+        = ::yarp::dev::DriverCreatorOf<yarp::dev::gzyarp::ControlBoardDriver>;
+    yarp::dev::Drivers::factory().add(
+        new ControlBoardDriverCreator("gazebo_controlboard", "", "ControlBoardDriver"));
 
-    // // Get model scoped name
-    // m_modelScopedName = _ecm.Component<components::ModelScopedName>(_entity)->Data();
+    yarp::os::Property driver_properties;
+    bool wipe = false;
 
-    // // Get device scoped name
-    // m_deviceScopedName = _sdf->Get<std::string>("device_scoped_name");
+    if (_sdf->HasElement("yarpConfigurationFile"))
+    {
+        std::string ini_file_path = _sdf->Get<std::string>("yarpConfigurationFile");
+        driver_properties.fromConfigFile(ini_file_path.c_str(), wipe);
+        if (!driver_properties.check("yarpDeviceName"))
+        {
+            yError() << "gz-sim-yarp-controlboard-system : missing yarpDeviceName parameter";
+            return;
+        }
 
-    // // Get model entity
-    // m_modelEntity = _ecm.EntityByComponents(components::ModelScopedName(m_modelScopedName));
+        yInfo() << "gz-sim-yarp-controlboard-system: configuration of device "
+                << driver_properties.find("yarpDeviceName").asString()
+                << " loaded from yarpConfigurationFile : " << ini_file_path << "\n";
+    } else
+    {
+        yError() << "gz-sim-yarp-controlboard-system : missing yarpConfigurationFile element";
+        return;
+    }
 
-    // // Get control board data
-    // m_ControlBoardData =
-    // ControlBoardDataSingleton::getControlBoardHandler()->getControlBoardData();
+    std::string deviceName = driver_properties.find("yarpDeviceName").asString();
+    m_robotScopedName = gz::sim::scopedName(_entity, _ecm, "/");
+    m_deviceScopedName
+        = m_robotScopedName + "/" + driver_properties.find("yarpDeviceName").asString();
+    m_modelEntity = _entity;
 
-    // // Register device
-    // if (!m_deviceRegistered)
-    // {
-    //     Handler::getHandler()->addDevice(m_deviceScopedName, m_ControlBoardData);
-    //     m_deviceRegistered = true;
-    // }
+    m_controlBoardData.modelScopedName = m_robotScopedName;
 
-    // // Open control board driver
-    // if (!m_controlBoardDriver.isValid())
-    // {
-    //     m_controlBoardDriver.open(m_deviceScopedName);
+    driver_properties.put(yarp::dev::gzyarp::YarpControlBoardScopedName.c_str(),
+                          m_robotScopedName.c_str());
+
+    // Insert the pointer in the singleton handler for retrieving it in the yarp driver
+    ControlBoardDataSingleton::getControlBoardHandler()->setControlBoardData(&(m_controlBoardData));
+
+    driver_properties.put("robot", m_robotScopedName);
+    driver_properties.put("device", m_deviceScopedName);
+
+    if (!m_controlBoardDriver.open(driver_properties))
+    {
+        yError() << "gz-sim-yarp-controlboard-system Plugin failed: error in opening yarp "
+                    "driver";
+        return;
+    }
+
+    if (!Handler::getHandler()->setDevice(m_deviceScopedName, &m_controlBoardDriver))
+    {
+        yError() << "gz-sim-yarp-basestate-system: failed setting scopedDeviceName(="
+                 << m_deviceScopedName << ")";
+        return;
+    }
+
+    m_deviceRegistered = true;
+    yInfo() << "Registered YARP device with instance name:" << m_deviceScopedName;
 }
 
 // pre-update
