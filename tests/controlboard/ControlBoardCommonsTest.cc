4c7c4d67ff165ad8fe6c4730ef5cf852350fcf52
Alessandro Croci <alessandro.croci@iit.it>
2024-03-08 14:37:54 +0100

    Add controlboard unit test for multiple control boards management
    Test failing for incorrect key used for controlboards

--- tests/controlboard/ControlBoardCommonsTest.cc
+++ tests/controlboard/ControlBoardCommonsTest.cc
@@ -1,29 +1,23 @@
-#include "../../libraries/common/Common.hh"
-#include "../../libraries/singleton-devices/Handler.hh"
-
-#include <cmath>
-#include <cstdlib>
-#include <iostream>
-#include <string>
+#include <ControlBoardDataSingleton.hh>
+#include <Handler.hh>
 
 #include <gtest/gtest.h>
+
+#include <algorithm>
+#include <string>
+
 #include <gz/sim/Joint.hh>
-#include <gz/sim/Link.hh>
-#include <gz/sim/Model.hh>
 #include <gz/sim/TestFixture.hh>
-#include <gz/sim/Util.hh>
 #include <gz/sim/World.hh>
-#include <gz/sim/components/JointForceCmd.hh>
 
 #include <yarp/dev/IControlMode.h>
 #include <yarp/dev/IEncoders.h>
 #include <yarp/dev/IPositionControl.h>
-#include <yarp/dev/IPositionDirect.h>
-#include <yarp/dev/ITorqueControl.h>
-#include <yarp/dev/PolyDriver.h>
-#include <yarp/os/Bottle.h>
-#include <yarp/os/BufferedPort.h>
-#include <yarp/os/Network.h>
+
+namespace gzyarp
+{
+namespace test
+{
 
 // Checks that the control board can be configured without initial conditions
 TEST(ControlBoardCommonsTest, ConfigureControlBoardWithoutInitialCondition)
@@ -46,3 +40,39 @@ TEST(ControlBoardCommonsTest, ConfigureControlBoardWithInitialCondition)
 
     testFixture.Finalize();
 }
+
+// Check that multiple control board can be congfigured for the same robot model
+TEST(ControlBoardCommonsTest, ConfigureMultipleControlBoards)
+{
+    std::string modelSdfName = "coupled_pendulum_two_controlboards.sdf";
+    std::string sdfPath = std::string("../../../tests/controlboard/") + modelSdfName;
+    std::string deviceScopedName = "model/coupled_pendulum/controlboard_plugin_device";
+    std::string cb1Key = "model/coupled_pendulum/controlboard_plugin_device";
+    std::string cb2Key = "model/coupled_pendulum/controlboard_plugin_device2";
+
+    bool configured = false;
+    gz::sim::Entity modelEntity;
+    gz::sim::Model model;
+    yarp::dev::PolyDriver* driver;
+
+    gz::sim::TestFixture testFixture{sdfPath};
+
+    gz::common::Console::SetVerbosity(4);
+
+    testFixture.Finalize();
+
+    // testFixture.Server()->Run(true, 1, false);
+
+    auto singleton = ControlBoardDataSingleton::getControlBoardHandler();
+    auto keys = singleton->getControlBoardKeys();
+
+    std::cerr << "ControlBoard singleton keys vector size: " << keys.size() << std::endl;
+
+    ASSERT_TRUE(configured);
+    ASSERT_EQ(keys.size(), 2);
+    ASSERT_TRUE(std::find(keys.begin(), keys.end(), cb1Key) != keys.end());
+    ASSERT_TRUE(std::find(keys.begin(), keys.end(), cb2Key) != keys.end());
+}
+
+} // namespace test
+} // namespace gzyarp
