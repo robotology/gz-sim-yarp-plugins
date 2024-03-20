48da91c54c420e3d3a1f257423040b95333f5fc4
Alessandro Croci <alessandro.croci@iit.it>
2024-03-20 16:55:05 +0100

    Update ClockTest

--- tests/clock/ClockTest.cc
+++ tests/clock/ClockTest.cc
@@ -4,7 +4,7 @@
 #include <yarp/os/BufferedPort.h>
 #include <yarp/os/Network.h>
 
-TEST(ClockTest, GetSimTimeAsExpected)
+TEST(ClockTest, GetSimulationTimeFromClockPort)
 {
     // ARRANGE
     yarp::os::NetworkBase::setLocalMode(true);
@@ -12,12 +12,14 @@ TEST(ClockTest, GetSimTimeAsExpected)
     gz::common::Console::SetVerbosity(4);
     // Instantiate test fixture
     gz::sim::TestFixture fixture("../../../tests/clock/model.sdf");
-    const int iterations = 10;
+    const int iterations = 1000;
     const int deltaTns = 1e6; // 1ms
     const int tolerance = 1e6; // 1ms
     yarp::os::BufferedPort<yarp::os::Bottle> p; // Create a port.
     p.open("/tmp_in"); // Give it a name on the network.
     yarp::os::Network::connect("/clock", "/tmp_in"); // connect two ports.
+    int expectedSimTimeSeconds = (iterations - 1) / 1e3;
+    int expectedSimTimeNanoseconds = 0;
 
     // ACT
     fixture.Server()->Run(/*_blocking=*/true, iterations, /*_paused=*/false);
@@ -25,8 +27,9 @@ TEST(ClockTest, GetSimTimeAsExpected)
 
     // ASSERT
     yarp::os::Bottle* b = p.read();
-    auto simTime = b->get(0).asInt64();
+    auto simTimeSeconds = b->get(0).asInt32();
+    auto simTimeNanoseconds = b->get(1).asInt32();
 
-    ASSERT_NEAR(simTime, (iterations - 1) * deltaTns, tolerance)
-        << "simTime: " << simTime << std::endl;
+    ASSERT_NEAR(simTimeSeconds, expectedSimTimeSeconds, tolerance);
+    ASSERT_NEAR(simTimeNanoseconds, expectedSimTimeNanoseconds, tolerance);
 }
