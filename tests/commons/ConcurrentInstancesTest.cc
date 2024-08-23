62498e558f02f79561c133fb05b4d7a8486e8135
Silvio Traversaro <silvio@traversaro.it>
2024-08-23 16:07:32 +0200

    Add workaround for "ODE INTERNAL ERROR 1: assertion "!colliders_initialized" failed in dInitColliders() [collision_kernel.cpp:168]" crash

--- tests/commons/ConcurrentInstancesTest.cc
+++ tests/commons/ConcurrentInstancesTest.cc
@@ -13,22 +13,30 @@ TEST(ConcurrentInstancesTest, StartConcurrentGazeboInstancesOfDifferentModels)
 {
     auto plannedIterations = 1'000;
 
-    gz::sim::TestFixture fixture1(
-        (std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / "dummy_sphere.sdf").string());
-    gz::sim::TestFixture fixture2(
-        (std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / "dummy_box.sdf").string());
     gz::common::Console::SetVerbosity(4);
 
+
+    gz::sim::TestFixture fixture1(
+        (std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / "dummy_sphere.sdf").string());
     fixture1.Finalize();
+
+    // Workaround for https://github.com/robotology/gz-sim-yarp-plugins/issues/201
+
+    gz::sim::TestFixture fixture2(
+        (std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR) / "dummy_box.sdf").string());
     fixture2.Finalize();
 
-    ASSERT_TRUE(fixture1.Server()->Run(false, plannedIterations, false));
-    ASSERT_TRUE(fixture2.Server()->Run(false, plannedIterations, false));
+    // First do a step blocked to workaround https://github.com/robotology/gz-sim-yarp-plugins/issues/20
+    // Remove if and once https://github.com/gazebosim/gz-physics/pull/675 is merged
+    ASSERT_TRUE(fixture1.Server()->Run(/*blocking=*/true, 1, /*paused=*/false));
+    ASSERT_TRUE(fixture2.Server()->Run(/*blocking=*/true, 1, /*paused=*/false));
+
+    ASSERT_TRUE(fixture1.Server()->Run(/*paused=*/false, plannedIterations-1, /*paused=*/false));
+    ASSERT_TRUE(fixture2.Server()->Run(/*paused=*/false, plannedIterations-1, /*paused=*/false));
 
     while (fixture1.Server()->Running() || fixture2.Server()->Running())
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        std::cerr << "Waiting for Gazebo simulation to finish..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     ASSERT_EQ(fixture1.Server()->IterationCount(), plannedIterations);
