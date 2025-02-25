ba46586dc2754c6bd964ed2cf7faf598e93cdfa0
Nicogene <nicolo.genesio@iit.it>
2025-02-25 15:59:21 +0100

    WIP

--- plugins/depthcamera/DepthCameraDriver.cpp
+++ plugins/depthcamera/DepthCameraDriver.cpp
@@ -39,18 +39,6 @@ public:
         m_display_time_box = false;
         m_display_timestamp = false;
         counter = 0;
-        sprintf(num[0].data, "**** ** ** ****");
-        sprintf(num[1].data, " *  *  *  *  * ");
-        sprintf(num[2].data, "***  *****  ***");
-        sprintf(num[3].data, "***  ****  ****");
-        sprintf(num[4].data, "* ** ****  *  *");
-        sprintf(num[5].data, "****  ***  ****");
-        sprintf(num[6].data, "****  **** ****");
-        sprintf(num[7].data, "***  *  *  *  *");
-        sprintf(num[8].data, "**** ***** ****");
-        sprintf(num[9].data, "**** ****  ****");
-        sprintf(num[10].data, "               ");
-        sprintf(num[11].data, "          ** **");
     }
 
     virtual ~DepthCameraDriver()
@@ -79,7 +67,6 @@ public:
         return true;
     }
 
-    // IFRAMEGRABBER IMAGE
     bool getImage(yarp::sig::ImageOf<yarp::sig::PixelRgb>& _image) override
     {
         std::lock_guard<std::mutex> lock(m_sensorData->m_mutex);
@@ -176,91 +163,246 @@ public:
         return true;
     }
 
-    int height() const override
+    int getRgbHeight() const override
     {
         return m_sensorData->m_height;
     }
 
-    int width() const override
+    int getRgbWidth() const override
     {
         return m_sensorData->m_width;
     }
 
-    int getRawBufferSize()
+
+    bool setRgbResolution(int width, int height) override
     {
-        return m_sensorData->m_bufferSize;
+        m_depthCameraSensorPtr->DepthCamera()->SetImageHeight(height);
+        m_depthCameraSensorPtr->DepthCamera()->SetImageWidth(width);
+    
+        m_width  = width;
+        m_height = height;
+        return true;
     }
-
-    void print(unsigned char* pixbuf, int pixbuf_w, int pixbuf_h, int x, int y, char* s, int size)
+    
+    bool getRgbFOV(double& horizontalFov, double& verticalFov) const override
     {
-        int pixelsize = 5;
-        for (int i = 0; i < size; i++)
+        horizontalFov = m_depthCameraSensorPtr->DepthCamera()->HFOV().Degree();
+        verticalFov   = m_depthCameraSensorPtr->DepthCamera()->VFOV().Degree();
+        return true;
+    }
+    bool setRgbFOV(double horizontalFov, double verticalFov) override
+    {
+        ignition::math::Angle hFov;
+        hFov.Degree(horizontalFov);
+        m_depthCameraSensorPtr->DepthCamera()->SetHFOV(hFov);
+        yCWarning(GAZEBODEPTH) << "GazeboDepthCameraDriver: only horizontal fov set!";
+        return true;
+    }
+    bool getRgbMirroring(bool& mirror) const override
+    {
+        mirror = false;
+        return true;
+    }
+    bool setRgbMirroring(bool mirror) override
+    {
+        yCError(GAZEBODEPTH)  << "setRgbMirroring not implemented yet";
+        return false;
+    }
+    bool getRgbIntrinsicParam(Property& intrinsic) const override
+    {
+        return getDepthIntrinsicParam(intrinsic);
+    }
+    bool getRgbImage(FlexImage& rgbImage, Stamp* timeStamp) const override
+    {
+        if(!timeStamp)
         {
-            char* num_p = 0;
-            switch (s[i])
+            yCError(GAZEBODEPTH) << "timestamp pointer invalid!";
+            return false;
+        }
+    
+        std::lock_guard<std::mutex> lock(m_colorFrameMutex);
+    
+        if(m_width == 0 || m_height == 0)
+        {
+            yCError(GAZEBODEPTH)  << "gazebo returned an invalid image size";
+            return false;
+        }
+        rgbImage.setPixelCode(m_imageFormat);
+        rgbImage.resize(m_width, m_height);
+        memcpy(rgbImage.getRawImage(), m_imageFrame_Buffer, m_imageFrame_BufferSize);
+        timeStamp->update(this->m_depthCameraSensorPtr->LastUpdateTime().Double());
+    
+        return true;
+    }
+    
+    int getDepthHeight() const override
+    {
+        return m_height;
+    }
+    int getDepthWidth() const override
+    {
+        return m_width;
+    }
+    bool setDepthResolution(int width, int height) const override
+    {
+        return setRgbResolution(width, height);
+    }
+    bool getDepthFOV(double& horizontalFov, double& verticalFov) const override
+    {
+        return getRgbFOV(horizontalFov, verticalFov);
+    }
+    bool setDepthFOV(double horizontalFov, double verticalFov) override
+    {
+        return getRgbFOV(horizontalFov, verticalFov);
+    }
+    bool getDepthIntrinsicParam(Property& intrinsic) override
+    {
+        using namespace gazebo::rendering;
+    
+        Distortion*  distModel;
+        DepthCamera* camPtr;
+        Value        rectM;
+    
+        intrinsic.put("physFocalLength", 0.0);
+        camPtr = m_depthCameraSensorPtr->DepthCamera().get();
+        if(camPtr)
+        {
+            intrinsic.put("focalLengthX",    1. / camPtr->OgreCamera()->getPixelDisplayRatio());
+            intrinsic.put("focalLengthY",    1. / camPtr->OgreCamera()->getPixelDisplayRatio());
+            distModel = camPtr->LensDistortion().get();
+            if(distModel)
             {
-            case '0':
-                num_p = num[0].data;
-                break;
-            case '1':
-                num_p = num[1].data;
-                break;
-            case '2':
-                num_p = num[2].data;
-                break;
-            case '3':
-                num_p = num[3].data;
-                break;
-            case '4':
-                num_p = num[4].data;
-                break;
-            case '5':
-                num_p = num[5].data;
-                break;
-            case '6':
-                num_p = num[6].data;
-                break;
-            case '7':
-                num_p = num[7].data;
-                break;
-            case '8':
-                num_p = num[8].data;
-                break;
-            case '9':
-                num_p = num[9].data;
-                break;
-            case ' ':
-                num_p = num[10].data;
-                break;
-            case '.':
-                num_p = num[11].data;
-                break;
+                intrinsic.put("k1",              distModel->K1());
+                intrinsic.put("k2",              distModel->K2());
+                intrinsic.put("k3",              distModel->K3());
+                intrinsic.put("t1",              distModel->P1());
+                intrinsic.put("t2",              distModel->P2());
+                intrinsic.put("principalPointX", distModel->Center().X());
+                intrinsic.put("principalPointY", distModel->Center().Y());
             }
-
-            for (int yi = 0; yi < 5; yi++)
+            else
             {
-                for (int xi = 0; xi < 3; xi++)
-                {
-                    int ii = yi * 3 + xi;
-                    if (num_p[ii] == '*')
-                    {
-                        for (int r = yi * pixelsize; r < yi * pixelsize + pixelsize; r++)
-                        {
-                            int off = i * (pixelsize + 20);
-                            for (int c = xi * pixelsize + off; c < xi * pixelsize + pixelsize + off;
-                                 c++)
-                            {
-                                unsigned char* pixel = pixbuf + c * 3 + r * (pixbuf_w * 3);
-                                pixel[0] = 0;
-                                pixel[1] = 0;
-                                pixel[2] = 255;
-                            }
-                        }
-                    }
-                }
+                intrinsic.put("k1",              0.0);
+                intrinsic.put("k2",              0.0);
+                intrinsic.put("k3",              0.0);
+                intrinsic.put("t1",              0.0);
+                intrinsic.put("t2",              0.0);
+                intrinsic.put("principalPointX", m_width/2.0);
+                intrinsic.put("principalPointY", m_height/2.0);
+            }
+    
+        }
+        intrinsic.put("rectificationMatrix", rectM.makeList("1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0"));
+        intrinsic.put("distortionModel", "plumb_bob");
+        intrinsic.put("stamp", m_colorTimestamp.getTime());
+        return true;
+    }
+    
+    double getDepthAccuracy() const override
+    {
+        return 0.00001;
+    }
+    
+    bool setDepthAccuracy(double accuracy) override
+    {
+        yCError(GAZEBODEPTH)  << "impossible to set accuracy";
+        return false;
+    }
+    
+    bool getDepthClipPlanes(double& nearPlane, double& farPlane) const override
+    {
+        nearPlane = m_depthCameraSensorPtr->DepthCamera()->NearClip();
+        farPlane  = m_depthCameraSensorPtr->DepthCamera()->FarClip();
+        return true;
+    }
+    bool setDepthClipPlanes(double nearPlane, double farPlane) override
+    {
+        m_depthCameraSensorPtr->DepthCamera()->SetClipDist(nearPlane,farPlane);
+        return true;
+    }
+    bool getDepthMirroring(bool& mirror) const override
+    {
+        return getRgbMirroring(mirror);
+    }
+    bool setDepthMirroring(bool mirror) override
+    {
+        return setRgbMirroring(mirror);
+    }
+    bool getDepthImage(depthImageType& depthImage, Stamp* timeStamp) const override
+    {
+        if(!timeStamp)
+        {
+            yCError(GAZEBODEPTH)  << "gazeboDepthCameraDriver: timestamp pointer invalid!";
+            return false;
+        }
+    
+        std::lock_guard<std::mutex> lock(m_depthFrameMutex);
+    
+        if(m_width == 0 || m_height == 0)
+        {
+            yCError(GAZEBODEPTH)  << "gazebo returned an invalid image size";
+            return false;
+        }
+    
+        depthImage.resize(m_width, m_height);
+        //depthImage.setPixelCode(m_depthFormat);
+        if(!m_depthQuantizationEnabled) {
+            memcpy(depthImage.getRawImage(), m_depthFrame_Buffer, m_width * m_height * sizeof(float));
+        }
+        else {
+            float nearPlane = (float) m_depthCameraSensorPtr->DepthCamera()->NearClip();
+            float farPlane = (float) m_depthCameraSensorPtr->DepthCamera()->FarClip();
+    
+            int intTemp;
+            float value;
+            float powCoeff = pow(10.0f, (float) m_depthDecimalNum);
+    
+            auto pxPtr = reinterpret_cast<float*>(depthImage.getRawImage());
+            for(int i=0; i<m_height*m_width; i++){
+                value = m_depthFrame_Buffer[i];
+    
+                intTemp = (int) (value * powCoeff);
+                value = ((float) intTemp) / powCoeff;
+    
+                if (value < nearPlane) { value = nearPlane; }
+                if (value > farPlane) { value = farPlane; }
+    
+                *pxPtr = value;
+                pxPtr++;
             }
         }
+    
+        timeStamp->update(this->m_depthCameraSensorPtr->LastUpdateTime().Double());
+    
+        return true;
     }
+    bool getExtrinsicParam(yarp::sig::Matrix& extrinsic) const override
+    {
+        extrinsic.resize(4, 4);
+        extrinsic.zero();
+        extrinsic[1][1] = extrinsic[2][2] = extrinsic[3][3] = extrinsic[4][4] = 1;
+        return true;
+    }
+    bool getImages(FlexImage& colorFrame, depthImageType& depthFrame, Stamp* colorStamp, Stamp* depthStamp) const override
+    {
+        return getDepthImage(depthFrame, depthStamp) && getRgbImage(colorFrame, colorStamp);
+    }
+    
+    IRGBDSensor::RGBDSensor_status getSensorStatus() override
+    {
+        return m_depthCameraSensorPtr->IsActive() ? RGBD_SENSOR_OK_IN_USE : RGBD_SENSOR_NOT_READY;
+    }
+    std::string getLastErrorMsg(Stamp* timeStamp) override
+    {
+        if(timeStamp)
+        {
+            timeStamp->update(this->m_depthCameraSensorPtr->LastUpdateTime().Double());
+    
+        }
+        return m_error;
+    }
+
+
 
     // IDepthCameraData
 
@@ -278,9 +420,4 @@ private:
     bool m_display_time_box;
     bool m_display_timestamp;
 
-    struct txt_type
-    {
-        char data[16];
-    };
-    txt_type num[12];
 };
